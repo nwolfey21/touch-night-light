2f53fa7a3e8dcda6c5d677bf56340595c05339a2
Noah Wolfe <nwolfey21@gmail.com>
2017-10-29 21:58:30 -0400

    Working prototype alternates sending red and off udp packets on touches

--- touch-controller.py
+++ touch-controller.py
@@ -1,9 +1,22 @@
 import RPi.GPIO as GPIO
 import time
+import socket
+
+# addressing information of target
+IPADDR = '192.168.0.115'
+PORTNUM = 2390
+
+# initialize a socket
+# SOCK_DGRAM specifies that this is UDP
+#s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 0)
+
+# enter the data content of the UDP packet as hex
+RED 		= str(1023)+':'+str(0)+':'+str(0)
+OFF 		= str(0)+':'+str(0)+':'+str(0)
 
 GPIO.setmode(GPIO.BCM)
 
-#set the GPIO input pins
+#set the GPIO input pins for touch sensor
 pad0 = 22
 pad1 = 10
 pad2 = 9
@@ -16,12 +29,18 @@ GPIO.setup(pad2, GPIO.IN)
 GPIO.setup(pad3, GPIO.IN)
 GPIO.setup(pad4, GPIO.IN)
 
+#light switches
+light0 = False
+light1 = False
+light2 = False
+light3 = False
+light4 = False
+
 pad0alreadyPressed = False
 pad1alreadyPressed = False
 pad2alreadyPressed = False
 pad3alreadyPressed = False
 pad4alreadyPressed = False
-
 
 while True:
     pad0pressed = not GPIO.input(pad0)
@@ -31,7 +50,26 @@ while True:
     pad4pressed = not GPIO.input(pad4)
     
     if pad0pressed and not pad0alreadyPressed:
-        print "Pad 0 pressed"
+		if light0:
+			print "Pad 0 pressed. Turning Light on."
+			# connect the socket
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 0)
+			s.connect((IPADDR, PORTNUM))
+			# send the command
+			s.send(RED)
+			# close the socket
+			s.close()
+			light0 = False
+		else:
+			print "Pad 0 pressed. Turning Light off."
+			# connect the socket
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 0)
+			s.connect((IPADDR, PORTNUM))
+			# send the command
+			s.send(OFF)
+			# close the socket
+			s.close()
+			light0 = True
     pad0alreadyPressed = pad0pressed
 
     if pad1pressed and not pad1alreadyPressed:
